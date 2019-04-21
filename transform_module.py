1004387518e91a4a747c7bd3ab1c4405f185c967
Alexandre Henrique <alexandre.hsd@gmail.com>
2019-04-21 13:35:51 -0300

    Module to transform any input data into a tensorflow object data

--- transform_module.py
+++ transform_module.py
@@ -1,34 +1,29 @@
 import tensorflow as tf
-import pickle
+import glob
 
 # Creating PreProcess class
-class PreProcess:
+class preProcess:
     def __init__(self):
         self.files = None
         self.images = None
+        self.num_files = 0
     
-    def get_path(self, file_path):
-        self.files = tf.constant(file_path)
+    def get_images(self, file_path):
+        self.files = sorted(glob.glob(file_path))
+        self.num_files = len(self.files)
+        self.files = tf.constant(self.files)
 
-    def standardize(self):
-        image_string = tf.read_file(self.files)
+    def standardize(self, files):
+        image_string = tf.read_file(files)
         image_decoded = tf.image.decode_jpeg(image_string, channels=3)
         image_resized = tf.image.resize_images(image_decoded, [64, 64])
         self.images = tf.image.per_image_standardization(image_resized)
+
         return self.images
   
     def transform(self):
         transformed_data = tf.data.Dataset.from_tensor_slices(self.files)
-        transformed_data = transformed_data.map(self.standardize())
+        transformed_data = transformed_data.map(self.standardize)
+        transformed_data = transformed_data.batch(batch_size=self.num_files)
 
         return transformed_data
-
-image_transformer = PreProcess()
-
-with open('transformer', 'wb') as file:
-    pickle.dump(image_transformer , file)
-
-
-# prep = PreProcess(file_path)
-# prep = prep.standardize()
-# data = prep.transform()
